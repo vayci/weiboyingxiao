2da4dd4951e8106b34e784a686875cc0d3eebf66
wenbinke <wenbinke@gmail.com>
2015-05-17 21:36:20 +0200

    update demo images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,40 @@
-jQuery(document).ready(function() {
+$(function() {
 
-    // Active links
-    var sections = $("section"),
-        navigation_links = $("#navigation a");
-
-    sections.waypoint({
+    $("section").waypoint({
         handler: function(event, direction) {
-            var active_section;
+            var $activeSection = $(this);
+            if (direction === "up") {
+                $activeSection = $activeSection.prev();
+            }
 
-            active_section = $(this);
-            if (direction === "up") active_section = active_section.prev();
-
-            var active_link = $('#navigation a[href="#' + active_section.attr("id") + '"]');
-
-            navigation_links.parent().removeClass("active");
-            active_link.parent().addClass("active");
+            $("#navigation a").parent().removeClass("active");
+            $('#navigation a[href="#' + $activeSection.attr("id") + '"]')
+                .parent().addClass("active");
         },
         offset: '50%'
     });
 
-    // Right navigation collapses navbar in mobile view
-    $(function() {
-        $('.navbar-right a').on('click', function() {
-            if ($('.navbar-toggle').css('display') != 'none') {
-                $(".navbar-toggle").trigger("click");
-            }
-        });
-    });
-
     // Scroll to top
-    $('body').append('<div id="back-top"><span class="icon-arrow-up2"></span></div>');
+    $('body').append('<div id="back-top"><span class="glyphicon glyphicon-arrow-up"></span></div>');
     $("#back-top").hide();
 
-    // fade in #back-top
-    $(function() {
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > 100) {
-                $('#back-top').fadeIn();
-            } else {
-                $('#back-top').fadeOut();
-            }
-        });
-
-        // scroll body to 0px on click
-        $('#back-top').click(function() {
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
-
-        $('.carousel').carousel({
-            interval: 3500,
-            pause: "none"
-        });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#back-top').fadeIn();
+        } else {
+            $('#back-top').fadeOut();
+        }
     });
 
-    // IE 10 in Windows 8 and Windows Phone 8 fix
-    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-        var msViewportStyle = document.createElement('style')
-        msViewportStyle.appendChild(
-            document.createTextNode(
-                '@-ms-viewport{width:auto!important}'
-            )
-        )
-        document.querySelector('head').appendChild(msViewportStyle)
-    }
+    $('#back-top').click(function() {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
 
-    // Android stock browser fix
-    var nua = navigator.userAgent
-    var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
-    if (isAndroid) {
-        $('select.form-control').removeClass('form-control').css('width', '100%')
-    }
+    $('.carousel').carousel({
+        interval: 3500,
+        pause: "none"
+    });
 });
